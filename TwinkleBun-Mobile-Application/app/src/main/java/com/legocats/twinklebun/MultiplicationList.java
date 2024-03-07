67def5b14f7d2ee792f25028d0f366ad9b025a26
kavishdiv2000 <dikavish62@gmail.com>
2024-03-07 19:21:44 +0530

    multiplicationlist
    added a button in the multiplication list to test the multiplication game

--- TwinkleBun-Mobile-Application/app/src/main/java/com/legocats/twinklebun/MultiplicationList.java
+++ TwinkleBun-Mobile-Application/app/src/main/java/com/legocats/twinklebun/MultiplicationList.java
@@ -2,13 +2,31 @@ package com.legocats.twinklebun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+
+import java.util.concurrent.TimeUnit;
 
 public class MultiplicationList extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplication_list);
+
+        b1 = findViewById(R.id.game_btn);
+        b1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MultiplicationGame.class);
+            startActivity(intent);
+
+        });
+
+
+
     }
 }
