602ecedafda7aa09fef9d750ddf066ae44c10598
RumalEkanayake <126671973+RumalEkanayake@users.noreply.github.com>
2024-03-24 19:11:20 +0530

    Create the scholarship paper file and start to edit

--- TwinkleBun-Mobile-Application/app/src/main/java/com/legocats/twinklebun/ScholarshipPaperList.java
+++ TwinkleBun-Mobile-Application/app/src/main/java/com/legocats/twinklebun/ScholarshipPaperList.java
@@ -2,7 +2,11 @@ package com.legocats.twinklebun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.legocats.twinklebun.ui.StartPaper;
 
 public class ScholarshipPaperList extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ScholarshipPaperList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scholarship_paper_list);
+    }
+
+    public void startPaper(View view) {
+        Intent intent = new Intent(ScholarshipPaperList.this, StartPaper.class);
+        startActivity(intent);
+        finish();
     }
 }
