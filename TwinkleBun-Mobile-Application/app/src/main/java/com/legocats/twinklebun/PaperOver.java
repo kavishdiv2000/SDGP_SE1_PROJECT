b70312e1d707e061f9ffdbb3f66161ac7b86c825
RumalEkanayake <126671973+RumalEkanayake@users.noreply.github.com>
2024-03-28 04:10:44 +0530

    Crete the finishing part of the paper

---
+++ TwinkleBun-Mobile-Application/app/src/main/java/com/legocats/twinklebun/PaperOver.java
@@ -0,0 +1,22 @@
+package com.legocats.twinklebun;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaperOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.paper_over);
+    }
+
+    public void restart(View view) {
+    }
+
+    public void exit(View view) {
+    }
+}
