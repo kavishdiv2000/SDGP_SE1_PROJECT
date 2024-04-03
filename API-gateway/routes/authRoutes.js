745616761f4cfee783349454a7ef9d9043a2285f
kavishdiv2000 <dikavish62@gmail.com>
2024-04-03 21:02:17 +0530

    did a change

--- API-gateway/routes/authRoutes.js
+++ API-gateway/routes/authRoutes.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.get("/signin",(req,res)=>{
+    res.send("this is the get route for signin in twinklebun")
+})
 router.post('/login', authController.signin);
 router.post('/refresh', authController.authenticateRefreshToken);
 
