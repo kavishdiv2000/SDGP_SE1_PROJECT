9e24842232d12bee843c8114b391e798ba7cce53
kavishdiv2000 <dikavish62@gmail.com>
2024-04-03 17:33:23 +0530

    updated the code for leadershipboard

--- API-gateway/controller/leadershipBoardController.js
+++ API-gateway/controller/leadershipBoardController.js
@@ -20,9 +20,13 @@ const leadershipBoard = async (req, res) =>{
         }else{
             leaders.sort((a, b) => b.score - a.score);
             let index = leaders.findIndex(currentUser =>{ return currentUser.id+" "=== user._id+" ";});
-            if (index === -1 || (index+1) !== rank){
+            if (index === -1 ){
                 index = rank-1;
                 leaders.splice(index, 0, {"name":user.name, "overallScore":user.overallScore, "id":user._id})
+            }else if((index+1) !== rank){
+                leaders.splice(index, 1);
+                index = rank-1;
+                leaders.splice(index , 0, {"name":user.name, "overallScore":user.overallScore, "id":user._id});
             }
             return res.status(200).json({"leaders":leaders, "rank":rank, "index":index});
         }
